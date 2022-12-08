4d4ca5eeb8e07ebaee2173f1c6db45b3be264f8c
Leo <whereismoney@qq.com>
2022-12-08 14:43:55 +0800

    start.php页面增加trycatch，防止异常退出

--- start.php
+++ start.php
@@ -11,8 +11,12 @@ $authorization = '';
 $chatGpt = new ChatGpt($sessionToken, $authorization);
 
 do {
-    $input = readline('问问神奇海螺：');
-    echo "让我想想。\n";
-    $text = $chatGpt->ask($input);
-    echo $text . PHP_EOL . PHP_EOL;
+    try {
+        $input = readline('问问神奇海螺：');
+        echo "让我想想。\n";
+        $text = $chatGpt->ask($input);
+        echo $text . PHP_EOL . PHP_EOL;
+    } catch (\Exception $ex) {
+        echo $ex->getMessage() . PHP_EOL . PHP_EOL;
+    }
 } while (true);
