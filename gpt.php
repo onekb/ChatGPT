bf2bea25d38581449acfff57e803778e2d540f34
chenchi <chenchi@kuaishou.com>
2023-03-27 21:10:11 +0800

    凑合用版本
    Signed-off-by: chenchi <chenchi@kuaishou.com>

--- gpt.php
+++ gpt.php
@@ -36,19 +36,18 @@ if ($act == 'question') {
         $result = ask($input);
         $text = $result['choices'][0]['message']['content'];
 	    $ret = 0;
-	    echo $text;
     } catch (\Exception $e) {
         $text = '可能是因为网络原因或速率限制，请求中断，你可以再问一次。'; // It may be due to network reasons or rate limiting, the request is interrupted, you can ask again.
 	    $ret = 1;
 	    echo $e;
     }
-} else if ($act == 'adsf'){
     echo <<<END
-<form action="index.php" method="POST">
-<form>
-问题：<br>
-<input type="text" name="firstname">
+<form action="gpt.php" method="POST">
+$text
 <br>
+<form>
+问题：
+<input type="text" name="question">
 <input type="submit" value="提问"> </form>
 END;
 } else {
